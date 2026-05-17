799028303021bc69e6f0c233fc72b5b0b0ad4eb9
seoyeeon <105402849+seoyeeon@users.noreply.github.com>
2026-05-17 22:58:47 +0900

    [level 3] Title: 네트워크, Time: 0.03 ms, Memory: 3.94 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.cpp
+++ 프로그래머스/3/43162. 네트워크/네트워크.cpp
@@ -6,9 +6,10 @@ using namespace std;
 void dfs(int cur, vector<vector<int>> &computers, vector<bool> &visited){
     visited[cur] = true;
     
+    // 현재 컴퓨터와 연결된 모든 컴퓨터 확인
     for(int i = 0; i < computers.size(); i++){
         if(computers[cur][i] == 1 && !visited[i]){
-            dfs(i, computers, visited);
+            dfs(i, computers, visited); // 그 컴퓨터로 이동
         }
     }
 }
@@ -19,8 +20,8 @@ int solution(int n, vector<vector<int>> computers) {
     
     for(int i = 0; i < n; i++){
         if(!visited[i]){
-            dfs(i, computers, visited);
-            answer++;
+            dfs(i, computers, visited); // 이 컴퓨터와 연결된애들 전부 탐색
+            answer++; // 네크워크 하나 발견한 것!
         }
     }
     return answer;
