5cd3075450201d456168ddff54e729a6237a3c0b
seoyeeon <105402849+seoyeeon@users.noreply.github.com>
2026-05-18 15:38:47 +0900

    [level 2] Title: 소수 찾기, Time: 0.58 ms, Memory: 4 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.cpp
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.cpp
@@ -1,9 +1,45 @@
 #include <string>
 #include <vector>
+#include <set>
 
 using namespace std;
 
+set<int> s;
+
+bool isPrime(int n){
+    if(n < 2) return false;
+    for(int i = 2; i*i <= n; i++){
+        if(n % i == 0) return false;
+    }
+    return true;
+}
+
+void dfs(string current, string numbers, vector<bool> &visited){
+    // 숫자가 만들어졌다면
+    if(current != ""){
+        int num = stoi(current);
+        
+        // 소수면 저장
+        if(isPrime(num)){
+            s.insert(num);
+        }
+    }
+    
+    for(int i = 0; i < numbers.size(); i++){
+        if(visited[i]) continue;
+        
+        visited[i] = true;
+        
+        dfs(current + numbers[i], numbers, visited);
+        
+        visited[i] = false;
+    }
+}
+
 int solution(string numbers) {
-    int answer = 0;
-    return answer;
+    vector<bool> visited(numbers.size(), false);
+    
+    dfs("", numbers, visited);
+    
+    return s.size();
 }
