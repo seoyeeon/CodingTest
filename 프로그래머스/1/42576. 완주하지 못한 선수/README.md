a181342d2adf7ce741a5556cd46d4b0e9bd5a0a8
seoyeeon <105402849+seoyeeon@users.noreply.github.com>
2026-07-30 01:01:28 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 37.30 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=cpp) 
 
 ### 성능 요약
 
-메모리: 102 MB, 시간: 41.69 ms
+메모리: 33.3 MB, 시간: 37.30 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 05월 24일 15:54:35
+2026년 07월 30일 01:01:23
 
 ### 문제 설명
 
---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+string solution(vector<string> participant, vector<string> completion) {
+    unordered_map<string, int> count;
+    
+    for(string p : participant){
+        count[p]++;
+    }
+    
+    for(string c : completion){
+        count[c]--;
+    }
+    
+    for(auto a : count){
+        if(a.second > 0){
+            return a.first;
+        }
+    }
+}
